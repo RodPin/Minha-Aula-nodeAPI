eb61ae54acb8d088376c3d98369b37d7b747423c
RODRIGO <prodrigo@poli.ufrj.br>
2019-12-04 17:23:39 -0300

    done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StyleSheet, Button, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Button, View, Text, TouchableOpacity} from 'react-native';
 import axios from 'axios';
 
 const api = axios.create({
@@ -7,11 +7,13 @@ const api = axios.create({
 });
 
 const App = ({}) => {
+  const [lista, setLista] = useState([]);
   async function pingar() {
     try {
       const response = await api.get('/tarefas', {
         data: {},
       });
+      setLista(response.data);
       console.log(response.data);
     } catch (e) {
       console.log(e);
@@ -20,14 +22,30 @@ const App = ({}) => {
   return (
     <>
       <Button title="Pingar" onPress={() => pingar()}></Button>
-      <Text style={styles.myText}>Oi </Text>
+      {lista.length > 0 ? (
+        lista.map(each => {
+          return (
+            <TouchableOpacity onPress={() => alert(each.id)} key={each.id}>
+              <View style={styles.myView}>
+                <Text style={styles.myText}>{each.id}</Text>
+                <Text style={styles.myText}>{each.titulo}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })
+      ) : (
+        <Text>lista nao carregada</Text>
+      )}
     </>
   );
 };
 
 const styles = StyleSheet.create({
   myText: {
-    fontSize: 70,
+    fontSize: 20,
+  },
+  myView: {
+    backgroundColor: 'green',
   },
 });
 
