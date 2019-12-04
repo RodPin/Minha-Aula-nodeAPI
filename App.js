111ab34e81398db9be2b9e3a6a6ab182506a9633
RODRIGO <prodrigo@poli.ufrj.br>
2019-12-04 17:10:53 -0300

    pingando api

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {StyleSheet, Button, Text} from 'react-native';
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://10.30.6.45:8085/rest',
+});
 
 const App = ({}) => {
+  async function pingar() {
+    try {
+      const response = await api.get('/tarefas', {
+        data: {},
+      });
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <>
-      <Text>Oi </Text>
+      <Button title="Pingar" onPress={() => pingar()}></Button>
+      <Text style={styles.myText}>Oi </Text>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  myText: {
+    fontSize: 70,
+  },
+});
 
 export default App;
